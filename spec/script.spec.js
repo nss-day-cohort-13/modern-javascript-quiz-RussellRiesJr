c18a65225111d5c7cf599a339c49c82acf4ab96c
Russell Ries <riesru@gmail.com>
2016-05-31 13:18:07 -0500

    add bower, jquery

---
+++ spec/script.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe("Robot funcionality testing", function() {
+
+  var testBot = new robot;
+
+  it("should exist", function() {
+    expect(testBot).toBeDefined()
+  });
+
+
+
+})
